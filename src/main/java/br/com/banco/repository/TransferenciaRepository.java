b5cdd4f745d85eb97153ab6e0bbc3a1f1583746c
Norton Manfre <nortonmanfrejr@hotmail.com>
2022-12-22 03:42:11 -0300

    22122022 -fix multiply inserts into data.sql for test

--- src/main/java/br/com/banco/repository/TransferenciaRepository.java
+++ src/main/java/br/com/banco/repository/TransferenciaRepository.java
@@ -1,6 +1,7 @@
 package br.com.banco.repository;
 
 import br.com.banco.model.Transferencia;
+import com.fasterxml.jackson.annotation.JsonKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
